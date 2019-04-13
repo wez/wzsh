ad8e82c4529e64e5eded05b3c4eef04ece0f8e34
Wez Furlong <wez@wezfurlong.org>
2019-04-13 07:27:08 -0700

    Add basic tab completion and hinting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use failure::{bail, format_err, Error, Fallible};
+use rustyline::completion::{Completer, FilenameCompleter, Pair};
 use rustyline::error::ReadlineError;
-use rustyline::Editor;
+use rustyline::highlight::Highlighter;
+use rustyline::hint::Hinter;
+use rustyline::{Config, Editor, Helper};
+use std::borrow::Cow;
 use std::process::Command;
 
 use shlex::string::ShellString;
@@ -26,11 +30,56 @@ impl Expander for ShellExpander {
     }
 }
 
-fn main() -> Result<(), Error> {
-    let mut rl = Editor::<()>::new();
-    if rl.load_history("history.txt").is_err() {
-        println!("No previous history.");
+struct LineEditorHelper {
+    completer: FilenameCompleter,
+}
+
+impl Completer for LineEditorHelper {
+    type Candidate = Pair;
+
+    fn complete(&self, line: &str, pos: usize) -> Result<(usize, Vec<Pair>), ReadlineError> {
+        self.completer.complete(line, pos)
     }
+}
+
+impl Hinter for LineEditorHelper {
+    fn hint(&self, line: &str, _pos: usize) -> Option<String> {
+        if line == "ls" {
+            Some(" -l".to_owned())
+        } else {
+            None
+        }
+    }
+}
+
+impl Highlighter for LineEditorHelper {
+    fn highlight_prompt<'p>(&self, prompt: &'p str) -> Cow<'p, str> {
+        Cow::Borrowed(prompt)
+    }
+
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Cow::Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
+    }
+
+    fn highlight<'l>(&self, line: &'l str, _pos: usize) -> Cow<'l, str> {
+        Cow::Borrowed(line)
+    }
+
+    fn highlight_char(&self, _line: &str, _pos: usize) -> bool {
+        true
+    }
+}
+
+impl Helper for LineEditorHelper {}
+
+fn main() -> Result<(), Error> {
+    let config = Config::builder().history_ignore_space(true).build();
+
+    let mut rl = Editor::with_config(config);
+    rl.set_helper(Some(LineEditorHelper {
+        completer: FilenameCompleter::new(),
+    }));
+    rl.load_history("history.txt").ok();
 
     let mut env = Environment::new();
     let expander = ShellExpander {};
@@ -62,11 +111,10 @@ fn main() -> Result<(), Error> {
                 }
             }
             Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                break;
+                // println!("CTRL-C");
+                continue;
             }
             Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
                 break;
             }
             Err(err) => {
