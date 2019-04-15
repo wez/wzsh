b913a93c9e4c5898a739a0f7860ada0333a857ee
Wez Furlong <wez@wezfurlong.org>
2019-04-15 07:47:43 -0700

    ShellExpander -> own module

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,18 @@
-use failure::{bail, format_err, Error, Fallible};
-
-use shlex::string::ShellString;
-use shlex::{Environment, Expander};
+use failure::Fallible;
 
 mod errorprint;
 mod execenv;
 mod exitstatus;
+mod expander;
 mod filedescriptor;
 mod parse;
 mod repl;
 
 use execenv::ExecutionEnvironment;
+use expander::ShellExpander;
 use repl::repl;
 
-pub struct ShellExpander {}
-impl Expander for ShellExpander {
-    fn lookup_homedir(
-        &self,
-        user: Option<&str>,
-        environment: &mut Environment,
-    ) -> Fallible<ShellString> {
-        if user.is_none() {
-            let home = environment
-                .get("HOME")
-                .ok_or_else(|| format_err!("HOME is not set"))?;
-            Ok(home.as_os_str().into())
-        } else {
-            bail!("ze goggles");
-        }
-    }
-}
-
-fn main() -> Result<(), Error> {
+fn main() -> Fallible<()> {
     let env = ExecutionEnvironment::new()?;
     let expander = ShellExpander {};
     repl(env, expander)
