e92ec3539439f8e0157375e1aaba54b2e37dfe62
Wez Furlong <wez@wezfurlong.org>
2019-12-16 08:29:44 -0800

    Allow running a script
    Hook up command line argument parsing (`wzsh -h` now does what you
    expect!) so that you can run a script:
    
    ```
    $ echo ls > /tmp/foo.wzsh
    $ ./target/debug/wzsh /tmp/foo.wzsh
    [output from ls shows here]
    ```
    
    There are probably things missing like correctly propagating the exit
    status from the script.
    
    Refs: https://github.com/wez/wzsh/issues/2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use crate::shellhost::FunctionRegistry;
 use shell_vm::Environment;
 use std::path::PathBuf;
 use std::sync::Arc;
+use structopt::StructOpt;
 
 mod builtins;
 mod errorprint;
@@ -11,6 +12,22 @@ mod job;
 mod repl;
 mod script;
 mod shellhost;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Wez's Shell\nhttp://github.com/wez/wzsh")]
+#[structopt(raw(
+    global_setting = "structopt::clap::AppSettings::ColoredHelp",
+    version = r#"env!("VERGEN_SEMVER_LIGHTWEIGHT")"#,
+))]
+struct Opt {
+    /// Skip loading startup.wzsh
+    #[structopt(long = "no-startup")]
+    skip_startup: bool,
+
+    /// Instead of starting the interactive REPL, load script
+    /// from file and execute it
+    file: Option<PathBuf>,
+}
 
 fn config_dir() -> PathBuf {
     dirs::home_dir()
@@ -24,15 +41,28 @@ fn main() -> anyhow::Result<()> {
     let mut env = Environment::new();
     let funcs = Arc::new(FunctionRegistry::new());
 
-    let startup_script = config_dir().join("startup.wzsh");
-    if startup_script.exists() {
-        if let Err(err) =
-            script::compile_and_run_script_file(&startup_script, &mut cwd, &mut env, &funcs)
-        {
-            print_error_path(&err, &startup_script);
-            eprintln!("wzsh: ignoring error during startup processing.");
+    let opts = Opt::from_args();
+    if !opts.skip_startup {
+        let startup_script = config_dir().join("startup.wzsh");
+        if startup_script.exists() {
+            if let Err(err) =
+                script::compile_and_run_script_file(&startup_script, &mut cwd, &mut env, &funcs)
+            {
+                print_error_path(&err, &startup_script);
+                eprintln!("wzsh: ignoring error during startup processing.");
+            }
         }
     }
 
-    repl::repl(cwd, env, &funcs)
+    if let Some(file) = opts.file.as_ref() {
+        if let Err(err) =
+            script::compile_and_run_script_file(file, &mut cwd, &mut env, &funcs)
+        {
+            print_error_path(&err, file);
+            std::process::exit(1);
+        }
+        Ok(())
+    } else {
+        repl::repl(cwd, env, &funcs)
+    }
 }
